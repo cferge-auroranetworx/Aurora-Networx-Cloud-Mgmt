54cc79868391b20593847aea7fd6fefbf795c629
Chad F <cferge@gmail.com>
2018-05-23 15:32:43 -0400

    Initial commit

---
+++ inc/settings.php
@@ -0,0 +1,14 @@
+<?php
+
+// Master Login System
+// Mihai Ionut Vilcu (ionutvmi@gmail.com)
+// configuration file
+
+
+// database details
+$set->db_host = 'localhost'; // database host
+$set->db_user = 'insideadmin'; // database user
+$set->db_pass = 'flipz429#'; // database password
+$set->db_name = 'insideadmin'; // database name
+
+define('MLS_PREFIX', 'mls_');
