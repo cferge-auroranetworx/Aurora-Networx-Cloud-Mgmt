91f794fee2d7b05ec03cd4ad693b9cb5e5c04b15
Chad F <cferge@gmail.com>
2018-05-24 11:27:26 -0400

    Good.

--- scripts/andc1-processor.py
+++ scripts/andc1-processor.py
@@ -3,10 +3,10 @@
 
 import os, platform, subprocess, re, sys
 
-print "This is the name of the script: ", sys.argv[0]
-print "Number of arguments: ", len(sys.argv)
-print "The arguments are: " , str(sys.argv)
-
+# print "This is the name of the script: ", sys.argv[0]
+# print "Number of arguments: ", len(sys.argv)
+# print "The arguments are: " , str(sys.argv)
+print 'Aurora Networx DC1 Processor v1.0'
 def get_processor_name():
     if platform.system() == "Linux":
         command = "cat /proc/cpuinfo"
@@ -24,9 +24,14 @@ with open('/proc/uptime', 'r') as f:
 
 print "\nSystem Uptime: ", (uptime_string)
 
+def check_availability (arg1):
+    host = str(sys.argv[0])
+    command = "ping", arg1
+    return
+check_availability(sys.argv[0])
 
 
-
-
-
+print 'FQDN :', gethostbyname
+print 'Disk Usage :'
+print 'Kernel Version : '
 print get_processor_name()
